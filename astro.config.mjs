e177c203c3bed601146bb0e04159ac9d18167a02
vcastr <victor_josafat.castro@daimlertruck.com>
2024-03-08 08:57:39 -0600

    ci: :rocket: Update astro.config.mjs with base, site, and trailingSlash options

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  base: "/marjeda-web-site",
+  base: '/marjeda-web-site',
+  site: 'https://bigcreator-dev.github.io/marjeda-web-site/',
+  trailingSlash: "never",
 });
